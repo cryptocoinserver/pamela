cd8cbf6043af1baf83d4e3edc96f864b47ab4e06
Markus K <16636744+cryptocoinserver@users.noreply.github.com>
2020-12-27 19:15:33 +0100

    Update deps.

--- mix.exs
+++ mix.exs
@@ -33,15 +33,15 @@ defmodule Pamela.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.9"},
-      {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_ecto, "~> 4.0"},
-      {:ecto_sql, "~> 3.1"},
-      {:postgrex, ">= 0.0.0"},
-      {:gettext, "~> 0.11"},
-      {:plug_cowboy, "~> 2.0"},
-      {:timex, "~> 3.1"},
-      {:nadia, "~> 0.6.0"},
+      {:phoenix, "~> 1.5.7"},
+      {:phoenix_pubsub, "~> 2.0.0"},
+      {:phoenix_ecto, "~> 4.2.1"},
+      {:ecto_sql, "~> 3.5.3"},
+      {:postgrex, ">= 0.15.7"},
+      {:gettext, "~> 0.18.2"},
+      {:plug_cowboy, "~> 2.4.1"},
+      {:timex, "~> 3.6.3"},
+      {:nadia, "~> 0.7.0"},
       {:binance, "~> 0.7.1"}
     ]
   end
